f4f8aafd97d3ad89f81e61ebae6a0277ab64fc17
Rooney-Eli <rooney.developer@gmail.com>
2024-03-19 15:23:49 -0600

    Initial Commit

---
+++ Checkers/CheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckersApp.swift
+//  Checkers
+//
+//  Created by Eli Rooney on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
